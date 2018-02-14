26fdc8bbf065c6c191d755285978644f3eb71c84
Sayan Raychaudhuri <Raychaudhurisayan@gmail.com>
2018-02-13 20:39:47 -0500

    Add files via upload

---
+++ Factorials.java
@@ -0,0 +1,11 @@
+public class Factorials {
+
+public static void main(String[] args) {
+		int factorial = 1;
+		for(int i=1;i<=10000;i++) {
+			factorial = factorial*i;
+				System.out.println(factorial); 
+		} 
+	}
+
+}

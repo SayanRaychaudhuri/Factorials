14fa76e1a2515188d2a12ece8064cef6c315cdfd
SayanRaychaudhuri <Raychaudhurisayan@gmail.com>
2018-02-14 20:35:20 -0500

    Recursive factorial function

--- Factorials.java
+++ Factorials.java
@@ -1,11 +1,14 @@
 public class Factorials {
 
-public static void main(String[] args) {
-		int factorial = 1;
-		for(int i=1;i<=10000;i++) {
-			factorial = factorial*i;
-				System.out.println(factorial); 
-		} 
-	}
+	public static void main(String[] args) {
+			System.out.println(factorial(10));
+		}
 
+	public static int factorial(int n){
+		if (n==0){
+			return 1;
+		} else {
+			return n * factorial(n-1);
+		}
+	}
 }
